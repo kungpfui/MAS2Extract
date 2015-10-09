ee2e5194999f0861bd8ac26e0e278c37644d7e24
kungpfui <kungpfui@gmail.com>
2015-10-09 13:35:49 +0200

    -change/improved the options handling of the program. It is now a little bit more "traditional". -target directory is created by default -default target directory is based on sourcefile but with .masdir extension instead of just the current working directory.

--- MAS2Extract/Program.cs
+++ MAS2Extract/Program.cs
@@ -1,47 +1,90 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace MAS2Extract
 {
     class Program
     {
+        static string usageText =
+@"mas2extract.exe [<options>] <filename.MAS> [<target-directory>]
+MAS2 extractor tool for rFactor 2 MAS game files.
+
+options:                
+   -s: will only show all files inside the archive, but not extract them
+   -c: do not create target-directory
+
+If no extract directory is entered, the location of mas2extract.exe will be used.
+
+Example show content only:
+  mas2extract.exe -s CorvettePC_CAR.mas
+Example rextract content into new created CorvettePC_CAR.masdir directory:
+  mas2extract.exe CorvettePC_CAR.mas
+";        
+        
         static void Main(string[] args)
         {
 
             if (args.Length == 0)
             {
-                Console.WriteLine("mas2extract.exe filename.MAS [-s] [target_directory]");
-                Console.WriteLine("MAS2 extractor tool for rFactor 2 MAS game files.");
-                Console.WriteLine("");
-                Console.WriteLine("Parameter -s will only show all files inside the archive, but not extract them");
-                Console.WriteLine("If no extract directory is entered, the location of mas2extract.exe will be used.");
-                return;
-            }
-            var f= args[0];
-            if(File.Exists(f) == false)
-            {
-                Console.WriteLine("Cannot find target MAS file!");
+                Console.WriteLine(usageText);
+                Environment.ExitCode = 1; // Parameter Error
                 return;
             }
 
-            string target = (args.Length == 2) ? args[1] : "./";                
-            bool unpack = true;
-            if (target == "-s")
+            // parse the command line for options and other arguments
+            var options = new List<string>();
+            var arguments = new List<string>();
+
+            bool endOfOptions = false;
+            foreach (string arg in args)
             {
-                unpack = false;
-            }else
-            {
-                if (!Directory.Exists(target))
+                if (endOfOptions) arguments.Add(arg);
+                else if (arg[0] == '-') options.Add(arg.TrimStart(new char[] { '-' }));
+                else 
                 {
-                    Console.WriteLine("Target extract directory doesn't exist");
-                    return;
+                    arguments.Add(arg);
+                    endOfOptions = true;
+                }
+            }
+            
+            bool optionShowOnly = options.IndexOf("s") >= 0;
+            bool optionNotCreate = options.IndexOf("c") >= 0;
+            string sourceFile   = arguments.Count > 0 ? arguments[0] : null;
+            string targetDir    = arguments.Count > 1 ? arguments[1] : null;
+
+            if(sourceFile == null || !File.Exists(sourceFile))
+            {
+                Console.Error.WriteLine("Cannot find target MAS file!");
+                Environment.ExitCode = 2; // Runtime Error
+                return;
+            }
+            
+            if (!optionShowOnly)
+            {                                    
+                if(targetDir == null)
+                {
+                    // build targetDir from filename and create if not already exists
+                    targetDir = "./" + Path.GetFileNameWithoutExtension(sourceFile) + ".masdir";
+                    optionNotCreate = false; // true makes no sense and will be ignored
+                }
+
+                if (!Directory.Exists(targetDir))
+                {                    
+                    if (optionNotCreate)
+                    {
+                        Console.Error.WriteLine("Target extract directory doesn't exist");
+                        Environment.ExitCode = 2; // Runtime Error
+                        return;
+                    }
+                    Directory.CreateDirectory(targetDir);                    
                 }
             }
 
             MAS2Reader reader;
             try
             {
-                reader = new MAS2Reader(f);
+                reader = new MAS2Reader(sourceFile);
                 Console.WriteLine(reader.Count + " files in archive");
 
                 Console.WriteLine("+{0}+", new string('-', 77));
@@ -52,35 +95,49 @@ namespace MAS2Extract
                 foreach(MAS2File file in reader.Files)
                 {
                     double ratio = (file.UncompressedSize == 0) ? 0.0 : 1.0 - (double)file.CompressedSize / (double)file.UncompressedSize;
-                    Console.Write("| {0,-33} | {1,10} | {2,9} | {3,6:P1} | {4,5} |",
-                        LimitString(file.Filename, 33), file.CompressedSize, file.UncompressedSize, ratio, file.FileType);
-                    try
+                    Console.Write("| {0,-33} | {1,10} | {2,9} | {3,6:P1} | {5,1}{4,4} |",
+                        LimitString(file.Filename, 33), file.CompressedSize, file.UncompressedSize, ratio, file.FileType & ~0x10010,
+                        // "Z" = Zlib deflate, "M" = MAS2 encoded, " " = plain
+                        (file.FileType & 0x10) != 0 ? "Z" : ((file.FileType & 0x10000)!=0 ? "M" : " ")
+                    );
+
+                    if (!optionShowOnly)
                     {
-                        if (unpack)
-                            reader.ExtractFile(file, Path.Combine(target, file.Filename));
-                    }catch(Exception ex)
-                    {
-                        Console.Write("FAIL");
+                        try
+                        {
+                            reader.ExtractFile(file, Path.Combine(targetDir, file.Filename));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Write("FAIL");
+                        }
                     }
                     Console.WriteLine();
                 }
-                
-                Console.WriteLine(
-                    "+-----------------------------------------------------------------------------+");
+                Console.WriteLine("+{0}+", new string('-', 77));
+
                 Console.WriteLine("Press ENTER to terminate");
                 Console.ReadLine();
+                Environment.ExitCode = 0;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Failed to open MAS archive. Is it a rFactor 2 archive?");
+                Console.Error.WriteLine("Failed to open MAS archive. Is it a rFactor 2 archive?");
+                Environment.ExitCode = 2;
             }
         }
 
 
-        private static string LimitString(string name, int max_width)
+        /// <summary>
+        /// Limit string length
+        /// </summary>
+        /// <param name="s">string to strip</param>
+        /// <param name="max_width">maximum number of characters</param>
+        /// <returns>stripped string</returns>
+        private static string LimitString(string s, int maxWidth)
         {
-            if (name.Length <= max_width) return name;            
-            return name.Substring(0, max_width - 3) + "...";            
+            if (s.Length <= maxWidth) return s;
+            return s.Substring(0, maxWidth - 3) + "...";            
         }
 
     }
